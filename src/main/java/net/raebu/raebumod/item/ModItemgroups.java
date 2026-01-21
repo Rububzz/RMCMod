5e39f803189064ce8209b4f4c896d458bb7ea2a1
rubinnn <lin.rubin2@gmail.com>
2026-01-21 14:15:39 +0800

    Add Pink_garnett_item_group

--- src/main/java/net/raebu/raebumod/item/ModItemgroups.java
+++ src/main/java/net/raebu/raebumod/item/ModItemgroups.java
@@ -1,8 +1,22 @@
 package net.raebu.raebumod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.raebu.raebumod.RaebuMod;
 
 public class ModItemgroups {
+    public static final ItemGroup PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP, Identifier.of(RaebuMod.MOD_ID, "pink_garnet_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_GARNET)).displayName(Text.translatable("itemgroup.raebumod.pink_garnet_items")).entries((displayContext, entries) ->{
+                entries.add(ModItems.PINK_GARNET);
+                entries.add(ModItems.RAW_PINK_GARNET);
+
+            } ).build());
+
     public static void registerItemGroups() {
         RaebuMod.LOGGER.info("Register Item Groups for " + RaebuMod.MOD_ID);
     }
